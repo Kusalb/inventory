5640ef15811ee425b924227d9987347208a576ee
abhishek-a19k <abhishek.a19k@gmail.com>
2018-05-09 17:34:47 +0545

    modal, controller

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Inventory;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+    public function index()
+    {
+        $inventory = Inventory::all();
+        return view('inventory.index',compact('inventory'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('inventory.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'roll_no' => 'required | unique:inventory',
+            'name' => 'required | string',
+        ]);
+        $input = $request->all();
+        $inventory = new  Inventory();
+        $inventory->name = $input['name'];
+        $inventory->roll_no = $input['quantity'];
+        $inventory->save();
+        return redirect('/inventory/index');
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $inventory=Inventory::find($id);
+        return view('inventory.edit', compact('inventory','id'));
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $student= Inventory::findorfail($id);
+        $input = $request->all();
+        $student->update($input);
+        return redirect('inventory/index');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $inventory = Inventory::find($id);
+        $inventory->delete();
+        return redirect('inventory/index');
+    }
+
+}
